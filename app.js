01867a8b70537aa019ba89bcc284fa7d5cde8be7
Jayveer Singh <30889815+jayyveer@users.noreply.github.com>
2022-07-22 15:56:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -133,10 +133,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-let port = process.env.PORT;
-if(port == null || port = ""){
-  port = 3000;
-}
-app.listen(port, function() {
-  console.log("Server started Succesfully");
-});
+app.listen(process.env.PORT  || 3000,function(){
+    console.log("Server is running on port 3000");
+})
